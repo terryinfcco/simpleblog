9ecf5baed14581ab3e2a05874a8c6d80ba6fc7b4
Terry Dutcher <terryinclmich@hotmail.com>
2023-11-03 11:04:00 -0600

    Partway through Video 2

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#     return render(request, 'home.html', {})
+
+# Going to use class based views.
+# Inherit the built in Django form ListView to be able to create a list of posts
+class HomeView(ListView):
+    # Tell Django what model we're using
+    model = Post
+    template_name = 'home.html'
+
+# Inherit the built in Django form DetailView to allow us to see a post's detail
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_detail.html'
