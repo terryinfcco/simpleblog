4e9788e47dd6b5ced1c80b93ece904958627c61b
Terry E. Dutcher <terryinclmich@hotmail.com>
2024-01-11 10:02:32 -0700

    Finished Video 2

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import HomeView, ArticleDetailView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # path('', views.home, name="home"),
+    # path for class based view
+    path('', HomeView.as_view(), name="home"),
+    # pk is primary key of each blog entry
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+# Import Django's generic listview and detailview
+from django.views.generic import ListView, DetailView
+# Have to import our model so the views can find it
+from .models import Post
+
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#     return render(request, 'home.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
